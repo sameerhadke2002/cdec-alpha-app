1ddc6bf70c917922759add244c1f0f754b2df11d
sameer hadke <Sameerhadke51@gmail.com>
2026-05-30 18:03:21 +0530

    Update S3 backend configuration for Terraform

--- infrastructure/backend/backend.tf
+++ infrastructure/backend/backend.tf
@@ -6,9 +6,9 @@
 
 terraform {
   backend "s3" {
-    bucket  = "cdec-alpha-terraform-state"
-    key     = "backend/terraform.tfstate"
-    region  = "us-east-1"
+    bucket = "sameer-terraform-state"
+    key = "eks/terraform.tfstate"
+    region = "us-east-1"
     encrypt = true
 
   }
