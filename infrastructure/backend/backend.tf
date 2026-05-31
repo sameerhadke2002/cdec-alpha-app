5dd6d479b51b0d8caf6ea1a869b236070bb1066e
sameer hadke <Sameerhadke51@gmail.com>
2026-05-31 12:31:58 +0530

    Change S3 bucket name for Terraform backend

--- infrastructure/backend/backend.tf
+++ infrastructure/backend/backend.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sameer-terraform-state"
+    bucket = "sameer-terraform-state2"
     key = "eks/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
